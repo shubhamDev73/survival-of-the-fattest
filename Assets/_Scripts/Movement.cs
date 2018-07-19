c151abfa1bc458937f8fbe9ebdac0e779159c0da
Shubham Gupta <shubham0209@gmail.com>
2018-07-20 01:03:29 +0530

    Corrected colliders

--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -10,16 +10,17 @@ public class Movement : MonoBehaviour {
 
 	private float speed = 100f, bulletSpeed = 2000f;
 	private String p;
+	private Rigidbody rb;
 	[@HideInInspector]
 	public Vector3 scale;
 
 	void Start () {
 		p = " Player" + player.ToString();
 		scale = transform.localScale;
+		rb = GetComponent<Rigidbody>();
 	}
 
 	void FixedUpdate () {
-		Rigidbody rb = GetComponent<Rigidbody>();
 		rb.AddForce(new Vector3(Input.GetAxis("Horizontal" + p) * speed, 0, -Input.GetAxis("Vertical" + p) * speed), ForceMode.Acceleration);
 		transform.localScale = Vector3.Lerp(transform.localScale, scale, (transform.localScale - scale).magnitude * 0.5f);
 		transform.position = new Vector3(transform.position.x, transform.localScale.x/2, transform.position.z);
@@ -32,7 +33,6 @@ public class Movement : MonoBehaviour {
 			bullet.transform.parent = GameObject.Find("Bullets").transform;
 			bullet.GetComponent<Bullet>().shooter = gameObject;
 			bullet.GetComponent<Rigidbody>().AddForce(bulletDirection * bulletSpeed);
-			Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GameObject.Find("Divider").GetComponent<Collider>());
 			Destroy(bullet, 2);
 			scale -= new Vector3(0.1f, 0.1f, 0.1f);
 		}
@@ -43,6 +43,18 @@ public class Movement : MonoBehaviour {
 
 	void Die () {
 		Destroy(gameObject);
+	}
+
+	void OnTriggerEnter (Collider col) {
+		switch(col.gameObject.tag){
+			case "Grinder":
+				Die();
+				break;
+			case "Farm":
+				Debug.Log(rb.velocity);
+				rb.AddForce(rb.velocity * -3, ForceMode.VelocityChange);
+				break;
+		}
 	}
 
 }
