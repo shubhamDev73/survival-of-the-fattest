b00605fbec0eb18eb92a5e4d0f704ce6018a4a58
Shubham Gupta <shubham0209@gmail.com>
2018-07-20 00:30:25 +0530

    Added bullet mechanics

---
+++ Assets/_Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject shooter;
+
+	void OnCollisionEnter (Collision col) {
+		if(col.gameObject != shooter){
+			if(col.gameObject.name == "Cow" || col.gameObject.name == "Pig"){
+				col.gameObject.GetComponent<Movement>().scale += new Vector3(0.25f, 0.25f, 0.25f);
+			}
+			Destroy(gameObject);
+		}
+	}
+
+}
--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	public int player;
-	public float speed, bulletSpeed;
 	public GameObject bulletPrefab;
 	public Transform spawnPoint;
 	public Vector3 bulletDirection;
 
+	private float speed = 100f, bulletSpeed = 2000f;
 	private String p;
+	[@HideInInspector]
+	public Vector3 scale = new Vector3(1, 1, 1);
 
 	void Start () {
 		p = " Player" + player.ToString();
@@ -17,16 +19,29 @@ public class Movement : MonoBehaviour {
 
 	void FixedUpdate () {
 		Rigidbody rb = GetComponent<Rigidbody>();
-		rb.AddForce(new Vector3(Input.GetAxis("Horizontal" + p) * speed, 0, -Input.GetAxis("Vertical" + p) * speed));
+		rb.AddForce(new Vector3(Input.GetAxis("Horizontal" + p) * speed, 0, -Input.GetAxis("Vertical" + p) * speed), ForceMode.Acceleration);
+		transform.localScale = Vector3.Lerp(transform.localScale, scale, (transform.localScale - scale).magnitude * 0.5f);
+		transform.position = new Vector3(transform.position.x, transform.localScale.x/2, transform.position.z);
 	}
 
 	void Update () {
 		if(Input.GetButtonDown("Fire" + p)){
-			GameObject bullet = Instantiate(bulletPrefab, spawnPoint);
+			GameObject bullet = Instantiate(bulletPrefab);
+			bullet.transform.position = spawnPoint.position;
 			bullet.transform.parent = GameObject.Find("Bullets").transform;
+			bullet.GetComponent<Bullet>().shooter = gameObject;
 			bullet.GetComponent<Rigidbody>().AddForce(bulletDirection * bulletSpeed);
 			Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GameObject.Find("Divider").GetComponent<Collider>());
 			Destroy(bullet, 2);
+			scale -= new Vector3(0.1f, 0.1f, 0.1f);
+		}
+		if(scale.x <= 0){
+			Die();
 		}
 	}
+
+	void Die () {
+		Destroy(gameObject);
+	}
+
 }
