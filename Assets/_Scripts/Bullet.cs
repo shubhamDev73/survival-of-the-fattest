660def287e21aa75bef68fb24a80244f486cd938
Shubham Gupta <shubham0209@gmail.com>
2018-07-20 02:46:47 +0530

    Corrected joystick

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -3,12 +3,15 @@
 public class Bullet : MonoBehaviour {
 
 	public GameObject shooter;
+	public GameObject explosionPrefab;
 
 	void OnCollisionEnter (Collision col) {
 		if(col.gameObject != shooter){
 			if(col.gameObject.name == "Cow" || col.gameObject.name == "Pig"){
 				col.gameObject.GetComponent<Movement>().scale += new Vector3(0.25f, 0.25f, 0.25f);
 			}
+			GameObject explosion = Instantiate(explosionPrefab, transform);
+			explosion.transform.parent = null;
 			Destroy(gameObject);
 		}
 	}
--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -9,40 +9,46 @@ public class Movement : MonoBehaviour {
 	public Vector3 bulletDirection;
 
 	private float speed = 100f, bulletSpeed = 2000f;
-	private String p;
 	private Rigidbody rb;
+	private String p;
 	[@HideInInspector]
 	public Vector3 scale;
 
 	void Start () {
-		p = " Player" + player.ToString();
+		p = player.ToString();
 		scale = transform.localScale;
 		rb = GetComponent<Rigidbody>();
 	}
 
 	void FixedUpdate () {
-		rb.AddForce(new Vector3(Input.GetAxis("Horizontal" + p) * speed, 0, -Input.GetAxis("Vertical" + p) * speed), ForceMode.Acceleration);
+		rb.AddForce(new Vector3(Input.GetAxis("Horizontal Player" + p) * speed, 0, -Input.GetAxis("Vertical Player" + p) * speed), ForceMode.Acceleration);
 		transform.localScale = Vector3.Lerp(transform.localScale, scale, (transform.localScale - scale).magnitude * 0.5f);
 		transform.position = new Vector3(transform.position.x, transform.localScale.x/2, transform.position.z);
+		speed = 200 - transform.localScale.x * 75;
 	}
 
 	void Update () {
-		if(Input.GetButtonDown("Fire" + p)){
-			GameObject bullet = Instantiate(bulletPrefab);
-			bullet.transform.position = spawnPoint.position;
-			bullet.transform.parent = GameObject.Find("Bullets").transform;
-			bullet.GetComponent<Bullet>().shooter = gameObject;
-			bullet.GetComponent<Rigidbody>().AddForce(bulletDirection * bulletSpeed);
-			Destroy(bullet, 2);
-			scale -= new Vector3(0.1f, 0.1f, 0.1f);
+		if(Input.GetKeyDown("joystick " + p + " button 2")){
+			Spawn(bulletSpeed, 2);
+			scale -= new Vector3(0.05f, 0.05f, 0.05f);
+		}
+		if(scale.x > 0.75f && Input.GetKeyDown("joystick " + p + " button 1")){
+			Spawn(bulletSpeed * 2, 30);
+			scale -= new Vector3(0.25f, 0.25f, 0.25f);
 		}
 		if(scale.x <= 0.4f){
 			Die();
 		}
 	}
 
-	void Die () {
-		Destroy(gameObject);
+	void Spawn (float bulletSpeed, float mass) {
+		GameObject bullet = Instantiate(bulletPrefab);
+		bullet.transform.position = spawnPoint.position;
+		bullet.GetComponent<Bullet>().shooter = gameObject;
+		Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GameObject.Find("Divider").GetComponent<Collider>());
+		bullet.GetComponent<Rigidbody>().mass = mass;
+		bullet.GetComponent<Rigidbody>().AddForce(bulletDirection * bulletSpeed, ForceMode.Acceleration);
+		Destroy(bullet, 2);
 	}
 
 	void OnTriggerEnter (Collider col) {
@@ -55,6 +61,10 @@ public class Movement : MonoBehaviour {
 				rb.AddForce(rb.velocity * -3, ForceMode.VelocityChange);
 				break;
 		}
+	}
+
+	void Die () {
+		Destroy(gameObject);
 	}
 
 }
